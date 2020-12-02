cd1b563a5082e83f6a6c32e71a2c37970ed34ce4
Tyler Harnach <harnatc@gmail.com>
2020-12-01 20:29:50 -0500

    day1 part 2

--- day1/index.js
+++ day1/index.js
@@ -3,11 +3,21 @@ import fs from 'fs'
 const stringArray = fs.readFileSync('./data.txt').toString().split("\r\n"); // windows line endings
 const numberArray = stringArray.map(item => Number(item)) // convert array to Numbers
 
+// for(let i = 0; i < numberArray.length; i++){
+//   for(let j = 0; j < numberArray.length; j++){
+//     if(numberArray[i] + numberArray[j] === 2020) {
+//       // two solutions are found, i * j, and j * i where they both equal 2020
+//       console.log(`Found: ${numberArray[i]} + ${numberArray[j]} = ${numberArray[i] * numberArray[j]}`)
+//     }
+//   }
+// }
+
 for(let i = 0; i < numberArray.length; i++){
   for(let j = 0; j < numberArray.length; j++){
-    if(numberArray[i] + numberArray[j] === 2020) {
-      // two solutions are found, i * j, and j * i where they both equal 2020
-      console.log(`Found: ${numberArray[i]} + ${numberArray[j]} = ${numberArray[i] * numberArray[j]}`)
+    for(let k = 0; k < numberArray.length; k++){
+      if(numberArray[i] + numberArray[j] + numberArray[k] === 2020) {
+        console.log(`Found: ${numberArray[i]} + ${numberArray[j]} + ${numberArray[k]} = ${numberArray[i] * numberArray[j] * numberArray[k]}`)
+      }
     }
   }
 }
